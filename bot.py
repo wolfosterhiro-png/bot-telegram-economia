4daf933eb075bb90f9341016e1cda8741a3eef75
wolfosterhiro-png <wolfosterhiro@gmail.com>
2026-02-09 23:41:34 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+# Base de datos temporal
+users = {}
+
+def get_user(user_id):
+    if user_id not in users:
+        users[user_id] = {
+            "money": 100
+        }
+    return users[user_id]
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user = get_user(message.from_user.id)
+    bot.reply_to(
+        message,
+        "🪙 Bienvenido al sistema económico de rol\n"
+        f"Dinero inicial: ${user['money']}\n\n"
+        "Comandos disponibles:\n"
+        "/balance – ver saldo\n"
+        "/work – trabajar"
+    )
+
+@bot.message_handler(commands=["balance"])
+def balance(message):
+    user = get_user(message.from_user.id)
+    bot.reply_to(
+        message,
+        f"💰 Tu saldo actual es: ${user['money']}"
+    )
+
+@bot.message_handler(commands=["work"])
+def work(message):
+    user = get_user(message.from_user.id)
+    user["money"] += 20
+    bot.reply_to(
+        message,
+        "🛠 Trabajaste y ganaste $20"
+    )
+
+print("Sistema económico activo...")
+bot.polling()
